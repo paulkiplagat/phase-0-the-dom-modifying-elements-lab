fe7cb920f3278fb9bac8880f8a2560135d946d28
Paul <paul.kiplagt@student.moringaschool.com>
2022-06-05 15:24:44 +0300

    commit tree 7eea240085821aeef5ce5ed40460b2dc93bd83f9

--- index.js
+++ index.js
@@ -1,10 +1,8 @@
-const testVar = "hi"
-
 /* Enter the code to remove the node element here */
 document.querySelector("main#main").remove();
 
 /* Create your new element here and assign it to newHeader */
 const newHeader = document.createElement("h1");
 newHeader.id = "victory"
-newHeader.innerHTML = "Flatiron School is the champion!";
+newHeader.innerHTML = "Paul Kiplagat is the champion!";
 newHeader.className = "victory";
